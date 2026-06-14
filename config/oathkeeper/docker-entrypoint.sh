fac65d612befa855afebfd434398969848168f82
Slavencer <javiercarrascoarango@gmail.com>
2026-06-14 13:12:49 +0200

    feat: removes the access-rules.yaml being generated on disk

--- config/oathkeeper/docker-entrypoint.sh
+++ config/oathkeeper/docker-entrypoint.sh
@@ -5,10 +5,10 @@ echo "Rendering access-rules.yaml from template..."
 
 envsubst '${DOMAIN_NAME} ${AUTH_DOMAIN} ${API_DOMAIN} ${OBJS_DOMAIN} ${DRAWIO_DOMAIN}' \
   < /etc/config/oathkeeper/access-rules.yaml.tpl \
-  > /etc/config/oathkeeper/access-rules.yaml
+  > /tmp/access-rules.yaml
 
 echo "Generated access rules:"
-cat /etc/config/oathkeeper/access-rules.yaml
+cat /tmp/access-rules.yaml
 
 echo "Starting Oathkeeper..."
 
